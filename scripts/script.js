a85b8cf3476e51698bdd975c667c8f4d881f15bb
jfonz412 <jfonzone@outlook.com>
2017-03-01 16:36:19 -0500

    figured out grid and pixel scaling

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,22 @@
+var gridSize = 1; //to double pixel amount, multiply by 4
+var pixelSize = 1; //to double pixel amount, divide in half
+
 $(document).ready(function(){
 
 	$('body').append('<div id="container"></div>');
-	$('container').append('<ul></ul>');
-	for(var i=0;i<256;i++)
+	for(var i=0;i<(256*gridSize);i++)
 	{
 		$('#container').append('<div class="pixel"></div>');
 	}
-
+	startDraw();
 });	
 
+var startDraw = function(){
+	/*start drawing*/
+	$('#container').click(function(){
+		$('.pixel').mouseenter(function(){
+			$(this).css('background-color','black');
+		});
+		
+	});
+};
